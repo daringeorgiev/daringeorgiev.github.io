4b1bf8d2d9aa091d5feb2871dd6e853e56dbaf10
darinnn <georgiev.darin@gmail.com>
2016-08-20 04:28:38 +0300

    Add go-top button

--- js/scripts.js
+++ js/scripts.js
@@ -27,6 +27,23 @@
                 });
             } // End if
         });
+
+        // Show or hide the sticky footer button
+        $(window).scroll(function() {
+            if ($(this).scrollTop() > 200) {
+                $('.go-top').fadeIn(500);
+            } else {
+                $('.go-top').fadeOut(300);
+            }
+        });
+
+        // Animate the scroll to top
+        $('.go-top').click(function(event) {
+            event.preventDefault();
+            $('html, body').animate({
+                scrollTop: 0
+            }, 800);
+        })
     });
 
     // Updating address bar on scroll
