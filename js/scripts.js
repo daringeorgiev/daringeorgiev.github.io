f82bc9b5e39ec3989782d62f28ea0d27ad176a51
Darin Georgiev <georgiev.darin@gmail.com>
2017-08-11 01:27:32 +0300

    Fix call of serviceWorker.js

--- js/scripts.js
+++ js/scripts.js
@@ -75,7 +75,7 @@
     // Add Service Worker
     if ('serviceWorker' in window.navigator) {
         window.navigator.serviceWorker
-            .register('/serviceWorker-min.js')
+            .register('/serviceWorker.js')
             .then(function() {
                 console.log("Service Worker Registered");
             });
