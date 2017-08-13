64e9c01a26998e9d3291779ca52d40c5ccdf2b27
Darin Georgiev <georgiev.darin@gmail.com>
2017-08-13 16:41:53 +0300

    Generate sourcemaps files, when uglify js files

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,11 @@
 var gulp = require('gulp');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
+var sourcemaps = require('gulp-sourcemaps');
 
 var paths = {
-    scripts: ['js/googleAnalytics.js', 'js/scripts.js', 'js/serviceworker-cache-polyfill.js']
+    scripts: ['js/googleAnalytics.js', 'js/scripts.js', 'js/serviceworker-cache-polyfill.js'],
+    maps: '../maps'
 };
 
 gulp.task('default', function () {
@@ -12,10 +14,12 @@ gulp.task('default', function () {
 
 gulp.task('scripts', function () {
     return gulp.src(paths.scripts)
+        .pipe(sourcemaps.init())
         .pipe(uglify())
         .pipe(rename({
             suffix: '-min'
         }))
+        .pipe(sourcemaps.write(paths.maps))
         .pipe(gulp.dest(function (file) {
             return file.base;
         }))
--- js/googleAnalytics-min.js
+++ js/googleAnalytics-min.js
@@ -1,1 +1,2 @@
 !function(e,t,a,n,c,o,s){e.GoogleAnalyticsObject=c,e[c]=e[c]||function(){(e[c].q=e[c].q||[]).push(arguments)},e[c].l=1*new Date,o=t.createElement(a),s=t.getElementsByTagName(a)[0],o.async=1,o.src="https://www.google-analytics.com/analytics.js",s.parentNode.insertBefore(o,s)}(window,document,"script",0,"ga"),ga("create","UA-83477833-1","auto"),ga("send","pageview");
+//# sourceMappingURL=../maps/googleAnalytics-min.js.map
