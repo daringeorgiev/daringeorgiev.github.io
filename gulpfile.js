7702a2c3790c407bb9aa293e49ce3be4f5f4543d
Darin Georgiev <georgiev.darin@gmail.com>
2017-08-14 00:11:35 +0300

    Use compressed css and add gulp task for css

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,12 @@ var gulp = require('gulp');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
 var sourcemaps = require('gulp-sourcemaps');
+var sass = require('gulp-sass');
+var autoprefixer  = require('gulp-autoprefixer');
 
 var paths = {
     scripts: ['js/googleAnalytics.js', 'js/scripts.js', 'js/serviceworker-cache-polyfill.js'],
+    css: 'css/styles.scss',
     maps: '../maps'
 };
 
@@ -16,6 +19,24 @@ gulp.task('scripts', function () {
     return gulp.src(paths.scripts)
         .pipe(sourcemaps.init())
         .pipe(uglify())
+        .pipe(rename({
+            suffix: '-min'
+        }))
+        .pipe(sourcemaps.write(paths.maps))
+        .pipe(gulp.dest(function (file) {
+            return file.base;
+        }))
+});
+
+gulp.task('css', function () {
+    return gulp.src(paths.css)
+        .pipe(sourcemaps.init())
+        .pipe(sass({
+            outputStyle: 'compressed'
+        }).on('error', sass.logError))
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions']
+        }))
         .pipe(rename({
             suffix: '-min'
         }))
