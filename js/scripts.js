c368cbe1993f1e99068548334ff4158ec04d4898
Darin Georgiev <georgiev.darin@gmail.com>
2017-08-24 00:19:56 +0300

    Defer images loading

--- js/scripts.js
+++ js/scripts.js
@@ -56,6 +56,16 @@
         });
     });
 
+    // Defer images loading
+    window.onload = function() {
+        var imgDefer = document.getElementsByTagName('img');
+        for (var i = 0; i < imgDefer.length; i++) {
+            if (imgDefer[i].getAttribute('data-src')) {
+                imgDefer[i].setAttribute('src', imgDefer[i].getAttribute('data-src'));
+            }
+        }
+    }
+
     // Update address bar, when scroll to top
     $(window).scroll(function() {
         if ($(this).scrollTop() === 0) {
